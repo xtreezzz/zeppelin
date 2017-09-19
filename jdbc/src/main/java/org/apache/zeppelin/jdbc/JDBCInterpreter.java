6598f6329a93c181e6a29f3ef36f0aa66fc2fc63
tinkoff-dwh <tinkoff.dwh@gmail.com>
2017-09-19 11:03:40 +0500

    precode for jdbc session

--- jdbc/src/main/java/org/apache/zeppelin/jdbc/JDBCInterpreter.java
+++ jdbc/src/main/java/org/apache/zeppelin/jdbc/JDBCInterpreter.java
@@ -106,6 +106,7 @@ public class JDBCInterpreter extends Interpreter {
   static final String JDBC_JCEKS_CREDENTIAL_KEY = "jceks.credentialKey";
   static final String DOT = ".";
   static final String PRECODE_KEY_TEMPLATE = "%s.precode";
+  static final String SESSION_PRECODE_KEY_TEMPLATE = "%s.sessionPrecode";
 
   private static final char WHITESPACE = ' ';
   private static final char NEWLINE = '\n';
@@ -581,6 +582,14 @@ public class JDBCInterpreter extends Interpreter {
 
         try {
           getJDBCConfiguration(user).saveStatement(paragraphId, statement);
+
+          String sessionPrecode =
+                  getProperty(String.format(SESSION_PRECODE_KEY_TEMPLATE, propertyKey));
+
+          if (StringUtils.isNotBlank(sessionPrecode)) {
+            System.out.println("!!!!!!!!! execute session precode " + sessionPrecode);
+            statement.execute(sessionPrecode);
+          }
 
           boolean isResultSetAvailable = statement.execute(sqlToExecute);
           getJDBCConfiguration(user).setConnectionInDBDriverPoolSuccessful(propertyKey);
