21bf6af3e1832dfeea4e9cfcf748948dae68e520
a.koshkin <andrey.a.koshkin@yandex.ru>
2019-03-13 11:44:24 +0300

    refactoring

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/interpreterV2/server/InterpreterProcessServerEventHandler.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/interpreterV2/server/InterpreterProcessServerEventHandler.java
@@ -1,27 +1,27 @@
-//package org.apache.zeppelin.interpreterV2.server;
-//
-//import org.apache.thrift.TException;
-//import org.apache.zeppelin.interpreter.core.thrift.OutputAppendEvent;
-//import org.apache.zeppelin.interpreter.core.thrift.RegisterInfo;
-//import org.apache.zeppelin.interpreter.core.thrift.RemoteInterpreterEventService;
-//
-//import java.util.function.Consumer;
-//
-//public class InterpreterProcessServerEventHandler  implements RemoteInterpreterEventService.Iface  {
-//
-//  private final Consumer<RegisterInfo> onStartInterpreterCallback;
-//
-//  public InterpreterProcessServerEventHandler(final Consumer<RegisterInfo> onStartInterpreterCallback) {
-//    this.onStartInterpreterCallback = onStartInterpreterCallback;
-//  }
-//
-//  @Override
-//  public void registerInterpreterProcess(final RegisterInfo registerInfo) throws TException {
-//    onStartInterpreterCallback.accept(registerInfo);
-//  }
-//
-//  @Override
-//  public void appendOutput(final OutputAppendEvent event) throws TException {
-//
-//  }
-//}
+package org.apache.zeppelin.interpreterV2.server;
+
+import org.apache.thrift.TException;
+import org.apache.zeppelin.interpreter.core.thrift.OutputAppendEvent;
+import org.apache.zeppelin.interpreter.core.thrift.RegisterInfo;
+import org.apache.zeppelin.interpreter.core.thrift.RemoteInterpreterEventService;
+
+import java.util.function.Consumer;
+
+public class InterpreterProcessServerEventHandler  implements RemoteInterpreterEventService.Iface  {
+
+  private final Consumer<RegisterInfo> onStartInterpreterCallback;
+
+  public InterpreterProcessServerEventHandler(final Consumer<RegisterInfo> onStartInterpreterCallback) {
+    this.onStartInterpreterCallback = onStartInterpreterCallback;
+  }
+
+  @Override
+  public void registerInterpreterProcess(final RegisterInfo registerInfo) throws TException {
+    onStartInterpreterCallback.accept(registerInfo);
+  }
+
+  @Override
+  public void appendOutput(final OutputAppendEvent event) throws TException {
+
+  }
+}
